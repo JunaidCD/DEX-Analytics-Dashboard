54511a5e5b88df24a72561ce41d2956cab1d0d45
JunaidCD <junaidmollah17@gmail.com>
2026-03-03 17:01:51 +0530

    feat: initialize Hardhat project with Paseo testnet config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,8 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-const config: HardhatUserConfig = {
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
   solidity: {
     version: "0.8.24",
     settings: {
@@ -27,5 +28,3 @@ const config: HardhatUserConfig = {
     }
   }
 };
-
-export default config;
--- test/Lock.js
+++ test/Lock.js
@@ -1,28 +1,27 @@
-import { expect } from "chai";
-import { ethers } from "hardhat";
-import { DEXToken } from "../typechain-types";
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-describe("DEXToken", () => {
-  let dexToken: DEXToken;
+describe("DEXToken", function () {
+  let dexToken;
 
-  beforeEach(async () => {
+  beforeEach(async function () {
     const [owner] = await ethers.getSigners();
     const dexTokenFactory = await ethers.getContractFactory("DEXToken");
-    dexToken = (await dexTokenFactory.deploy(ethers.parseEther("1000000"))) as DEXToken;
+    dexToken = await dexTokenFactory.deploy(ethers.parseEther("1000000"));
     await dexToken.waitForDeployment();
   });
 
-  it("should have correct name and symbol", async () => {
+  it("should have correct name and symbol", async function () {
     expect(await dexToken.name()).to.equal("DEX Token");
     expect(await dexToken.symbol()).to.equal("DEX");
   });
 
-  it("should mint initial supply to deployer", async () => {
+  it("should mint initial supply to deployer", async function () {
     const [owner] = await ethers.getSigners();
     expect(await dexToken.balanceOf(owner.address)).to.equal(ethers.parseEther("1000000"));
   });
 
-  it("should transfer correctly", async () => {
+  it("should transfer correctly", async function () {
     const [owner, addr1] = await ethers.getSigners();
     await dexToken.transfer(addr1.address, ethers.parseEther("100"));
     expect(await dexToken.balanceOf(addr1.address)).to.equal(ethers.parseEther("100"));
